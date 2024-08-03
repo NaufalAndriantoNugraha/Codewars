a6a365fded9cfbacb8065b1974039f0bbfc50eff
NaufalAndriantoNugraha <andriantonugrahanaufal@gmail.com>
2024-08-03 08:48:23 +0700

    Add 8kyu - Find the smallest integer in the array

---
+++ 8kyu/FindTheSamllestIntegerInTheArray.ts
@@ -0,0 +1,10 @@
+/**
+ * Title: Find the smallest integer in the array
+ * Difficulty: 8kyu
+ * Link: https://www.codewars.com/kata/55a2d7ebe362935a210000b2/typescript
+ * Language attempt: TypeScript
+ **/
+
+export function findSmallestInt(args: number[]) {
+  return Math.min(...args);
+}
