165c0b26539fd69a527f34be22598e4bbacabd0e
NaufalAndriantoNugraha <andriantonugrahanaufal@gmail.com>
2024-08-05 11:04:06 +0700

    Add 8kyu - MakeUpperCase

---
+++ 8kyu/MakeUpperCase.go
@@ -0,0 +1,14 @@
+/**
+ * Title: MakeUpperCase
+ * Difficulty: 8kyu
+ * Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/go
+ * Language attempt: Go
+ **/
+
+package kata
+
+import "strings"
+
+func MakeUpperCase(str string) string {
+	return strings.ToUpper(str)
+}
