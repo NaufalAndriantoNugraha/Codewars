4435b41047d0652a8539ae3f1cab748e5c42b7be
NaufalAndriantoNugraha <andriantonugrahanaufal@gmail.com>
2024-08-06 22:10:06 +0700

    Add 8kyu - Count Odd Numbers below n

---
+++ 8kyu/CountOddNumbersBelowN.go
@@ -0,0 +1,12 @@
+/**
+ * Title: Count Odd Numbers below n
+ * Difficulty: 8kyu
+ * Link: https://www.codewars.com/kata/59342039eb450e39970000a6/train/go
+ * Language attempt: Go
+ **/
+
+package kata
+
+func OddCount(input int) int {
+	return input / 2
+}
