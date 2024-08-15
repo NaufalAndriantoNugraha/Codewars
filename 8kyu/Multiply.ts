a3f35f74b1d25a597151ef9d088d5b96049d9174
NaufalAndriantoNugraha <andriantonugrahanaufal@gmail.com>
2024-08-15 11:50:20 +0700

    Add 8kyu - Multiply

---
+++ 8kyu/Multiply.ts
@@ -0,0 +1,10 @@
+/**
+ * Title: Multiply
+ * Difficulty: 8kyu
+ * Link: https://www.codewars.com/kata/50654ddff44f800200000004/typescript
+ * Language attempt: TypeScript
+ **/
+
+export function multiply(a: number, b: number) {
+  return a * b;
+}
